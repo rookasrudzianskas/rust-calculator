832836416186c6c9ab693c7f30e3843dddbb53f5
Rokas <rookas.rudzenskas@gmail.com>
2022-11-08 17:04:35 +0200

    Add support for division and multiplication
    This commit adds support for division and multiplication.
    
    The reason for this change is that the calculator should support all
    basic operations.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,27 @@ fn main() {
     let mut args: Args = args();
 
     let first = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
     let first_number = first.parse::<f32>().unwrap();
-    let second_number = second.parse::<f32>(().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+    let result = operate(operator, first_number, second_number);
 
-    println!("{:?} {} {}", first, operator, second);
+    println!("{:?}", result);
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     if operator == '+' {
-        return first_number + second_number;
+        first_number + second_number
     } else if  operator == '-' {
-        return first_number - second_number;
+        first_number - second_number
     } else if operator == '/' {
-        return first_number / second_number;
+        first_number / second_number
     } else if operator == '*' {
-        return first_number * second_number;
+        first_number * second_number
     } else {
-        panic!("Unknown operator");
+        // panic!("Unknown operator");
+        0.0
     }
 }
