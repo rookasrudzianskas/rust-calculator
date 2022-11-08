8559af6715ad4843f6c63e76f14741239d10dbba
Rokas <rookas.rudzenskas@gmail.com>
2022-11-08 17:00:07 +0200

    Add basic calculator functionality
    This commit adds the ability to add, subtract, multiply and divide two numbers.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,26 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
 
-    let first = args.nth(0);
-    println!("{:?}", first);
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>(().unwrap();
+
+    println!("{:?} {} {}", first, operator, second);
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    if operator == '+' {
+        return first_number + second_number;
+    } else if  operator == '-' {
+        return first_number - second_number;
+    } else if operator == '/' {
+        return first_number / second_number;
+    } else if operator == '*' {
+        return first_number * second_number;
+    } else {
+        panic!("Unknown operator");
+    }
 }
